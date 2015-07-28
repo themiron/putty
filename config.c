f05c7c7a7bf2372440ddb77fc1d911d8a4ade246
Vladislav Grishenko <themiron@mail.ru>
2015-07-28 17:34:23 +0500

    Expand a bit session listbox

--- config.c
+++ config.c
@@ -1404,7 +1404,7 @@ void setup_config_box(struct controlbox *b, int midsession,
 				HELPCTX(session_saved),
 				sessionsaver_handler, P(ssd));
     ssd->listbox->generic.column = 0;
-    ssd->listbox->listbox.height = 7;
+    ssd->listbox->listbox.height = 9;
     if (!midsession) {
 	ssd->loadbutton = ctrl_pushbutton(s, "Load", 'l',
 					  HELPCTX(session_saved),
